39a89a1ebf8cd7600b56f925a0562676f4140de0
p3te <p96.smith@qut.edu.au>
2021-10-14 13:20:37 +1000

    Updating the GetMessageFromPool to take a generic T : Message so that Activator.CreateInstance<T>() works as expected.

--- com.unity.robotics.ros-tcp-connector/Runtime/TcpConnector/ROSConnection.cs
+++ com.unity.robotics.ros-tcp-connector/Runtime/TcpConnector/ROSConnection.cs
@@ -1040,7 +1040,7 @@ namespace Unity.Robotics.ROSTCPConnector
             RosTopicState topicState = GetTopic(rosTopicName);
             if (topicState != null)
             {
-                return (T)topicState.GetMessageFromPool();
+                return topicState.GetMessageFromPool<T>();
             }
             throw new Exception($"No publisher on topic {rosTopicName} of type {MessageRegistry.GetRosMessageName<T>()} to get pooled messages from!");
         }
